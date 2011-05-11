8958cdccbff83556e91719d1ca5628b42a7c1d58
plukevdh <hungerandthirst@gmail.com>
2011-05-11 12:17:33 -0400

    get your damn requires straight.

--- lib/git-pivot.rb
+++ lib/git-pivot.rb
@@ -1,5 +1,6 @@
 module GitPivot
-  autoload :Git, 'git-pivot/git'
-  autoload :Pivotal, 'git-pivot/pivotal'
-  autoload :Manager, 'git-pivot/manager'
+  autoload :Git,      'git-pivot/git'
+  autoload :Pivotal,  'git-pivot/pivotal'
+  autoload :Manager,  'git-pivot/manager'
+  autoload :Shared,   'git-pivot/shared'
 end
